0c940429fcd98b0379c4ccfd6b7f1961670d20a9
Tristan ELAIN <tristan.elain@gmail.com>
2019-10-25 18:03:43 +0200

    docs(event-bus-resolver): add class jsdoc

--- src/event-bus/event-bus-resolver.js
+++ src/event-bus/event-bus-resolver.js
@@ -2,6 +2,10 @@ import { SocketEventBus } from './websockets/event-bus-websockets';
 import { SocketEventBusClient } from './websockets/event-bus-websockets-client';
 import { PostMessageEventBus } from './postmessage/event-bus-postmessage';
 
+/**
+ * @classdesc Instantiate event buses based on params given
+ * @class
+ */
 export class EventBusResolver {
     constructor(params) {
         if (params.server) {
