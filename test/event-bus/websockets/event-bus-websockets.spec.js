5d9f60aeaff9aabbe6b2b687062f099a0c152738
Tristan ELAIN <tristan.elain@gmail.com>
2019-10-25 10:37:42 +0200

    test(socket-event-bus): tests for SocketEventBus

---
+++ test/event-bus/websockets/event-bus-websockets.spec.js
@@ -0,0 +1,40 @@
+import { assert } from 'chai';
+import { spy } from 'sinon';
+import { SocketEventBus } from '../../../src/event-bus/websockets/event-bus-websockets';
+
+describe('SocketEventBus', function() {
+    let eventBus = new SocketEventBus();
+    beforeEach(function() {
+        eventBus = new SocketEventBus();
+    });
+
+    describe('on()', function() {
+        it('should fire events', function() {
+            // Given
+            const socketA = { on: spy() },
+                socketB = { on: spy() },
+                socketC = { on: spy() };
+            const key = 'test';
+            eventBus.sockets = [socketA, socketB, socketC];
+            // When
+            eventBus.on(key, () => key);
+            // Then
+            assert(socketA.on.calledOnceWith(key));
+            assert(socketB.on.calledOnceWith(key));
+            assert(socketC.on.calledOnceWith(key));
+        });
+    });
+
+    describe('emit()', function() {
+        it('sould broadcast the data', function() {
+            // Given
+            const socket = { broadcast: { emit: spy() } };
+            const key = 'test';
+            const message = 'message';
+            // When
+            eventBus.emit(key, message, socket);
+            // Then
+            assert(socket.broadcast.emit.calledOnceWith(message));
+        });
+    });
+});
