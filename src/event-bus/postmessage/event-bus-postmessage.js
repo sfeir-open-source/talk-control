36c40d06a53447c8ecb5dbe473e48bdca7b5355f
Tristan ELAIN <tristan.elain@gmail.com>
2019-10-25 16:55:20 +0200

    feat(event-bus-postmessage): add window based event-bus implementation
    #1

---
+++ src/event-bus/postmessage/event-bus-postmessage.js
@@ -0,0 +1,53 @@
+import { EventBus } from '../event-bus.js';
+
+/**
+ * @classdesc Event bus implementation that fire events through window
+ * @class
+ * @augments EventBus
+ */
+export class PostMessageEventBus extends EventBus {
+    /**
+     * Class constructor
+     */
+    constructor() {
+        super();
+        window.addEventListener('message', this._receiveMessageWindow.bind(this), false);
+    }
+
+    /**
+     * Emit data via window and for each callback registered on given event key
+     *
+     * @param {string} key - Event key to fire
+     * @param {any} data - Data to emit
+     * @throws Will throw an error if key is not specified
+     */
+    emit(key, data) {
+        super.emit(key, data);
+        // Inner broadcast (same app)
+        window.postMessage(
+            JSON.stringify({
+                type: key,
+                data
+            }),
+            '*'
+        );
+    }
+
+    /**
+     * Handle message reception from window
+     *
+     * @param {MessageEvent} message - message to forward
+     */
+    _receiveMessageWindow(message) {
+        if (!message || !message.data || message.data.length === 0) {
+            return;
+        }
+        if (message.data.charAt(0) === '{' && message.data.charAt(message.data.length - 1) === '}') {
+            message = JSON.parse(message.data);
+            const callBacks = super.getCallbacks(message.type);
+            if (callBacks && callBacks.length > 0) {
+                callBacks.forEach(callback => callback(message));
+            }
+        }
+    }
+}
