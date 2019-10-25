e7e56727e98a809d672ab9fee535dbb85f34f726
Tristan ELAIN <tristan.elain@gmail.com>
2019-10-25 17:01:01 +0200

    feat(event-bus-resolver): add EventBus resolver implementation

---
+++ src/event-bus/event-bus-resolver.js
@@ -0,0 +1,19 @@
+import { SocketEventBus } from './websockets/event-bus-websockets';
+import { SocketEventBusClient } from './websockets/event-bus-websockets-client';
+import { PostMessageEventBus } from './postmessage/event-bus-postmessage';
+
+export class EventBusResolver {
+    constructor(params) {
+        if (params.server) {
+            if (params.client) {
+                this.socketBus = new SocketEventBusClient(params.server);
+            } else {
+                this.socketBus = new SocketEventBus(params.server);
+            }
+        }
+
+        if (typeof window != 'undefined') {
+            this.postMessageBus = new PostMessageEventBus();
+        }
+    }
+}
