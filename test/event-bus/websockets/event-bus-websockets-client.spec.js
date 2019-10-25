7a17a7f27ffdeecb2bc0d2363cec37ef8a795bf9
Tristan ELAIN <tristan.elain@gmail.com>
2019-10-25 15:32:41 +0200

    fix(event-bus-ws-client): fix SocketEventBusClient implementation
    SocketEventBusClient implementation was wrongly extending SocketEventBus
    
    #1

--- test/event-bus/websockets/event-bus-websockets-client.spec.js
+++ test/event-bus/websockets/event-bus-websockets-client.spec.js
@@ -1,14 +1,51 @@
-import { expect } from 'chai';
+import { expect, assert } from 'chai';
 import { SocketEventBusClient } from '../../../src/event-bus/websockets/event-bus-websockets-client';
 import { stub, spy } from 'sinon';
 import socketIO from 'socket.io-client';
 
 describe('SocketEventBusClient', function() {
+    let eventBus;
+    before(function() {
+        stub(socketIO, 'connect').returns({ on: spy() });
+    });
+
+    beforeEach(function() {
+        eventBus = new SocketEventBusClient();
+    });
+
     describe('constructor()', function() {
         it('should have instantiated SocketEventBusClient', function() {
-            stub(socketIO, 'connect').returns({ on: spy() });
-            const eventBus = new SocketEventBusClient();
             expect(eventBus).to.be.ok;
+        });
+    });
+
+    describe('on()', function() {
+        it('should fire events', function() {
+            // Given
+            const socketA = { on: spy() },
+                socketB = { on: spy() },
+                socketC = { on: spy() };
+            const key = 'test';
+            eventBus.sockets = [socketA, socketB, socketC];
+            // When
+            eventBus.on(key, () => key);
+            // Then
+            assert(socketA.on.calledOnceWith(key));
+            assert(socketB.on.calledOnceWith(key));
+            assert(socketC.on.calledOnceWith(key));
+        });
+    });
+
+    describe('emit()', function() {
+        it('should broadcast the data', function() {
+            // Given
+            const key = 'test';
+            const data = 'data';
+            eventBus.io = { emit: spy() };
+            // When
+            eventBus.emit(key, data);
+            // Then
+            assert(eventBus.io.emit.calledOnceWith(data));
         });
     });
 });
