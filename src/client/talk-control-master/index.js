336251b07347df8f0a2722ef040ce96c5f3102b2
Gildas Morel des Vallons <gildas2k@gmail.com>
2019-10-31 16:36:15 +0100

    feat(talk-control-master): add entry point

---
+++ src/client/talk-control-master/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import TalkControlMaster from './talk-control-master.js';
+
+window.addEventListener('DOMContentLoaded', function() {
+    console.log('ready');
+    const talkControlMaster = new TalkControlMaster();
+    talkControlMaster.init();
+});
