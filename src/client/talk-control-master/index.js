43b1f0e0a697b0fe478417bba3e577930ca87c55
Gildas Morel des Vallons <gildas2k@gmail.com>
2019-10-31 18:07:30 +0100

    refactor(talk-control-master): remove console.log

--- src/client/talk-control-master/index.js
+++ src/client/talk-control-master/index.js
@@ -3,7 +3,6 @@
 import { TalkControlMaster } from './talk-control-master.js';
 
 window.addEventListener('DOMContentLoaded', function() {
-    console.log('ready');
     const talkControlMaster = new TalkControlMaster();
     talkControlMaster.init();
 });
