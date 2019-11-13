fb2846d1233161e8ce1b644f5dff5313278655d5
Tristan ELAIN <tristan.elain@gmail.com>
2019-11-13 15:32:51 +0100

    feat(tc-slave): add talk-control-slave implementation

---
+++ src/client/talk-control-slave/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+import { TalkControlSlave } from './talk-control-slave';
+
+// TODO: retrieve engineName from configuration
+new TalkControlSlave({ engineName: 'revealjs' });
--- src/client/talk-control-slave/talk-control-slave.js
+++ src/client/talk-control-slave/talk-control-slave.js
@@ -1,1 +1,23 @@
-// TODO ...
+'use strict';
+
+import { EventBusResolver } from '@event-bus/event-bus-resolver';
+import { EngineResolver } from '../engines/engine-resolver';
+
+export class TalkControlSlave {
+    constructor(params) {
+        this.eventBus = new EventBusResolver({
+            postMessage: {
+                slave: true
+            }
+        });
+        this.engine = EngineResolver.getEngine(params.engineName);
+        this.eventBus.postMessageBus.on('init', this.init.bind(this));
+    }
+
+    init() {
+        // Send the total slide number
+        const slides = this.engine.getSlides();
+        this.eventBus.postMessageBus.emit('initialized', { slides });
+        this.eventBus.postMessageBus.on('gotoSlide', data => this.engine.goToSlide(data.slide));
+    }
+}
