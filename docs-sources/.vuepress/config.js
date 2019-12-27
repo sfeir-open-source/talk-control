17c8df9cc0bb16c13a9c17c5e16d4632a2707d5a
Tristan ELAIN <tristan.elain@gmail.com>
2019-12-27 16:14:51 +0100

    docs: add vuepress config

---
+++ docs-sources/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: 'TalkControl',
+    description: 'TalkControl documentation',
+    themeConfig: {
+        nav: [
+            { text: 'User guide', link: '/users/'},
+            { text: 'Developer guide', link: '/developers/'},
+            { text: 'JsDoc', link: '/developers/code/' },
+          ],
+          editLinks: true,
+          sidebarDepth: 1,
+          docsDir: "code",
+  }
+}
