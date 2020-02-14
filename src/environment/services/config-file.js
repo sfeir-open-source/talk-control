6a4d0043a733de5ae5e9d359d868b19affaaf02c
Gildas Morel des Vallons <gildas2k@gmail.com>
2020-02-14 15:09:25 +0100

    feat: add create-tunnels script

---
+++ src/environment/services/config-file.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const fs = require('fs');
+
+const configFilePath = __dirname + '/../../../config/config.json';
+
+/**
+ * Update service item with external url in config file
+ *
+ * @param {string} item - service item in config (tc-server, tc-master, ...)
+ * @param {string} url - url external to write in config for service item
+ */
+exports.setExternalUrl = (item, url) => {
+    const config = require(configFilePath);
+    config[item].urls.external = url;
+    writeNewConfig(config);
+};
+
+/**
+ * Write config file with new config object
+ *
+ * @param {*} config - config object
+ */
+const writeNewConfig = config => {
+    fs.writeFileSync(configFilePath, JSON.stringify(config, null, 4));
+};
