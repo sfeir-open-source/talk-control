0824198fbabb861fe8d1f4412fe91e0d9990485d
Gildas Morel des Vallons <gildas2k@gmail.com>
2020-03-13 11:16:17 +0100

    feat: change the way url-form interract with tc-master home
    - Remove tc-url-form from on-stage and presenter view
    - Add tc-url-form to tc-master home
    - Handle edition and validation mode for tc-url-form
    - Show layouts buttons when url is validated

--- src/client/layouts/common/url-form/url-form.js
+++ src/client/layouts/common/url-form/url-form.js
@@ -14,31 +14,54 @@ class UrlForm extends LitElement {
 
     firstUpdated() {
         super.firstUpdated();
-        const inputPresentation = this.shadowRoot.getElementById('inputPresentation');
-        const validateButton = this.shadowRoot.getElementById('btnValidate');
-        const validateForm = () => {
-            const url = inputPresentation.value;
-            // If url invalid, show an error
-            this.shadowRoot.getElementById('urlError').classList.add('is-hidden');
-            if (!isUrlValid(url)) {
-                this.shadowRoot.getElementById('urlError').classList.remove('is-hidden');
-                return;
-            }
-            const urlChangedEvent = new CustomEvent('url-changed', {
-                detail: {
-                    url
-                }
-            });
-            dispatchEvent(urlChangedEvent);
-        };
-        // Start validation when button cliked or 'enter' pressed
-        validateButton.addEventListener('click', validateForm);
-        inputPresentation.addEventListener('keypress', e => {
+        const presentationUrl = this.shadowRoot.getElementById('presentationUrl');
+        const validateButton = this.shadowRoot.getElementById('validateButton');
+        const updateButton = this.shadowRoot.getElementById('updateButton');
+        const urlError = this.shadowRoot.getElementById('urlError');
+
+        // Presentation url field validation
+        presentationUrl.addEventListener('keypress', e => {
             const key = e.which || e.keyCode;
             if (key === 13) {
-                validateForm();
+                validateUrl();
             }
         });
+        validateButton.addEventListener('click', () => validateUrl());
+
+        // Update button click
+        updateButton.addEventListener('click', () => {
+            dispatchEvent(new CustomEvent('url-form-editing'));
+            switchToEdition();
+        });
+
+        const validateUrl = () => {
+            const url = presentationUrl.value;
+
+            hideUrlError();
+            if (!isUrlValid(url)) {
+                showUrlError();
+                return;
+            }
+
+            sessionStorage.setItem('presentationUrl', url);
+            dispatchEvent(new CustomEvent('url-form-validated'));
+            switchToReadOnly();
+        };
+
+        const switchToEdition = () => {
+            this.shadowRoot.getElementById('updateButton').classList.add('is-hidden');
+            this.shadowRoot.getElementById('validateButton').classList.remove('is-hidden');
+            this.shadowRoot.getElementById('presentationUrl').disabled = false;
+        };
+
+        const switchToReadOnly = () => {
+            this.shadowRoot.getElementById('validateButton').classList.add('is-hidden');
+            this.shadowRoot.getElementById('updateButton').classList.remove('is-hidden');
+            this.shadowRoot.getElementById('presentationUrl').disabled = true;
+        };
+
+        const hideUrlError = () => urlError.classList.add('is-hidden');
+        const showUrlError = () => urlError.classList.remove('is-hidden');
     }
 
     render() {
@@ -53,36 +76,30 @@ class UrlForm extends LitElement {
                     <!-- Enter presentation url form area -->
                     <div class="columns">
                         <div class="column is-half">
-                            <div class="columns">
-                                <div class="column is-four-fifths">
-                                    <input
-                                        class="input"
-                                        type="url"
-                                        placeholder="Enter the url of your presentation : http://my.presentation.url/index.html"
-                                        id="inputPresentation"
-                                        value="${isRemote ? config.tcShowcase.urls.external : config.tcShowcase.urls.local}"
-                                    />
-                                </div>
-                                <div class="column">
-                                    <button id="btnValidate" class="button">Validate Url</button>
-                                </div>
-                            </div>
+                            <input
+                                class="input"
+                                type="url"
+                                placeholder="Enter the url of your presentation : http://my.presentation.url/index.html"
+                                id="presentationUrl"
+                                value="${isRemote ? config.tcShowcase.urls.external : config.tcShowcase.urls.local}"
+                            />
+                        </div>
+                        <div class="column">
+                            <button id="validateButton" class="button">Validate</button>
+                            <button id="updateButton" class="button is-hidden">Edit</button>
                         </div>
                     </div>
 
                     <!-- Error message area -->
-                    <div class="columns">
-                        <div class="column is-half">
-                            <div id="urlError" class="notification is-danger is-hidden">
-                                <button class="delete" @click="${() => this.shadowRoot.getElementById('urlError').classList.add('is-hidden')}"></button>
-                                URL is not valid
-                            </div>
-                        </div>
+                    <div id="urlError" class="notification is-danger is-hidden">
+                        <button class="delete" @click="${() => this.shadowRoot.getElementById('urlError').classList.add('is-hidden')}"></button>
+                        URL is not valid
                     </div>
                 </div>
             </section>
         `;
     }
 }
+
 // Register the new element with the browser.
 customElements.define('tc-url-form', UrlForm);
