78027289e4de9f49cb975434e6722fbb1633a283
Gildas Morel des Vallons <gildas2k@gmail.com>
2020-07-24 18:08:53 +0200

    feat: add zoom feature with testing coordinates

--- src/client/layouts/common/slide-view/slide-view.js
+++ src/client/layouts/common/slide-view/slide-view.js
@@ -13,6 +13,7 @@ class SlideView extends LitElement {
             delta: { type: String, reflect: true, attribute: true },
             focus: { type: Boolean, reflect: true, attribute: true },
             fullscreen: { type: Boolean, reflect: true, attribute: true },
+            zooming: { type: Boolean, reflect: true, attribute: true },
             pointer: { type: Object, reflect: true, attribute: true }
         };
     }
@@ -36,6 +37,12 @@ class SlideView extends LitElement {
                     border-radius: 6px;
                     background-color: #FF0000
                 }
+                .zoomable { 
+                    width: 100%;
+                    height: 100%;
+                    transition-duration: 0.8s;
+                    cursor: zoom-in;
+                }
             `
         ];
     }
@@ -45,6 +52,7 @@ class SlideView extends LitElement {
         this.url = '';
         this.delta = '0';
         this.fullscreen = false;
+        this.zooming = false;
         this.pointer = { x: 0, y: 0, color: '#FF0000' };
     }
 
@@ -62,6 +70,11 @@ class SlideView extends LitElement {
 
             if (typeof message.data === 'object' && message.data.type === 'pointerColor') {
                 this._setPointerColor(message.data.color);
+            }
+            
+            if (typeof message.data === 'object' && message.data.type === 'pointerClick') {
+                // TODO : Set variables for values below with message.data.x and y and convert percents into pixels
+                this._toggleZoom(300, 200);
             }
         });
     }
@@ -83,7 +96,7 @@ class SlideView extends LitElement {
     render() {
         return html`
             <section style="width: ${this.fullscreen ? '100vw' : '100%'}; height: ${this.fullscreen ? '100vh' : '100%'}">
-                <iframe>Current slide</iframe>
+                <iframe id="zoomableElement" class="zoomable">Current slide</iframe>
                 <div id="pointer"/>
             </section>
         `;
@@ -96,6 +109,29 @@ class SlideView extends LitElement {
 
     _setPointerColor(color) {
         this.shadowRoot.getElementById('pointer').style.backgroundColor = this.pointer.color = color;
+    }
+
+    _toggleZoom(mouseX, mouseY) {
+        const element = this.shadowRoot.getElementById('zoomableElement');
+
+        if (this.zooming) {
+            element.style.transform = 'translate3D(0px, 0px, 0px)';
+            element.style.cursor = 'zoom-in';
+            this.zooming = !this.zooming;
+            return;
+        }
+
+        const scaleValue = 2;
+        const windowCenterX = window.innerWidth / 2;
+        const windowCenterY = window.innerHeight / 2;
+        const targetX = Math.round((windowCenterX - mouseX) * scaleValue);
+        const targetY = Math.round((windowCenterY - mouseY) * scaleValue);
+
+        if (!this.zooming) {
+            element.style.transform = `translateX(${targetX}px) translateY(${targetY}px) scale(${scaleValue})`;
+            element.style.cursor = 'zoom-out';
+            this.zooming = !this.zooming;
+        }
     }
 }
 // Register the new element with the browser.
