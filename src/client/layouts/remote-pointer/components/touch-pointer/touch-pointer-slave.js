7f2c5d60538833e85b3bcd5fbaf01d017e2a6598
Gildas Morel des Vallons <gildas2k@gmail.com>
2020-07-24 16:33:46 +0200

    fix: set empty init method to avoid js error

--- src/client/layouts/remote-pointer/components/touch-pointer/touch-pointer-slave.js
+++ src/client/layouts/remote-pointer/components/touch-pointer/touch-pointer-slave.js
@@ -4,6 +4,8 @@ import { MASTER_SLAVE_CHANNEL } from '@event-bus/event-bus-resolver';
 import { TalkControlSlave } from '@client/talk-control-slave/talk-control-slave';
 
 export class TouchPointerSlave extends TalkControlSlave {
+    init() {}
+
     sendPointerPositionToMaster(position) {
         this.eventBusSlave.emit(MASTER_SLAVE_CHANNEL, 'sendPointerPositionToMaster', position);
     }
