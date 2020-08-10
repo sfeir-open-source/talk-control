549f6c855722dbfc0d47fcd6111808424d83c00c
Gildas Morel des Vallons <gildas2k@gmail.com>
2020-08-10 17:13:17 +0200

    refactor: rename tcMaster config entry

--- src/client/tc-controller/index.js
+++ src/client/tc-controller/index.js
@@ -4,11 +4,11 @@ import config from '@config/config.json';
 const QRCode = require('qrcode');
 
 window.addEventListener('DOMContentLoaded', function() {
-    if (config.tcMaster.urls.external) {
-        QRCode.toCanvas(document.getElementById('qrCode'), config.tcMaster.urls.external);
+    if (config.tcController.urls.external) {
+        QRCode.toCanvas(document.getElementById('qrCode'), config.tcController.urls.external);
         document.getElementById(
             'textCode'
-        ).innerHTML = `<a href="${config.tcMaster.urls.external}" title="Use this url to connect to TalkControl from another device">${config.tcMaster.urls.external}</a>`;
+        ).innerHTML = `<a href="${config.tcController.urls.external}" title="Use this url to connect to TalkControl from another device">${config.tcController.urls.external}</a>`;
     } else {
         document.getElementById('qrCodeSection').classList.add('is-hidden');
     }
