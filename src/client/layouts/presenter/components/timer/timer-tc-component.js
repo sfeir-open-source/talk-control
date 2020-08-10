c938b6f869ed5f3431161f20f967854b0bb04da9
Gildas Morel des Vallons <gildas2k@gmail.com>
2020-08-10 17:28:57 +0200

    refactor: rename TimerSlave to TimerTCComponent

--- src/client/layouts/presenter/components/timer/timer-tc-component.js
+++ src/client/layouts/presenter/components/timer/timer-tc-component.js
@@ -1,6 +1,6 @@
 import { TCComponent } from '@client/tc-component/tc-component.js';
 
-export class TimerSlave extends TCComponent {
+export class TimerTCComponent extends TCComponent {
     init() {
         postMessage({ type: 'initTimer' });
     }
