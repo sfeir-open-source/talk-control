6c58b0f6a79fb13a9aac6d153c43b726f5e8c4c6
Gildas Morel des Vallons <gildas2k@gmail.com>
2020-08-10 17:25:38 +0200

    refactor: rename SlideViewSlave to SlideViewTCComponent

--- src/client/layouts/common/slide-view/slide-view-tc-component.js
+++ src/client/layouts/common/slide-view/slide-view-tc-component.js
@@ -6,7 +6,7 @@ import { TCComponent } from '@client/tc-component/tc-component';
 /**
  * Class to use plugins with SlideView component, and have access to eventBusComponent.
  */
-export class SlideViewSlave extends TCComponent {
+export class SlideViewTCComponent extends TCComponent {
     init() {
         this.eventBusComponent.on(CONTROLLER_COMPONENT_CHANNEL, 'registerPlugin', ({ pluginName }) => this.registerPlugin(pluginName));
     }
