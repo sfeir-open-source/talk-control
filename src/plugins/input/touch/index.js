5f63e1e9e7aefe33c58e9daf9452c37437ef4485
Gildas Morel des Vallons <gildas2k@gmail.com>
2020-09-07 12:04:37 +0200

    fix: check if target is presentation iframe when capturing touch event

--- src/plugins/input/touch/index.js
+++ src/plugins/input/touch/index.js
@@ -1,3 +1,5 @@
+import { isPresentationIframe } from '@services/context';
+
 class TouchInput {
     constructor() {
         this.usedByAComponent = true;
@@ -19,28 +21,30 @@ class TouchInput {
     }
 
     _captureTouchEvent(event, forward = false) {
-        this.touchPosition[event.type] = {
-            clientX: event.changedTouches[0].clientX,
-            clientY: event.changedTouches[0].clientY
-        };
-
-        if (forward) {
-            const xDiff = this.touchPosition.touchstart.clientX - this.touchPosition.touchend.clientX;
-            const yDiff = this.touchPosition.touchstart.clientY - this.touchPosition.touchend.clientY;
-            const xDiffPositive = xDiff < 0 ? xDiff * -1 : xDiff;
-            const yDiffPositive = yDiff < 0 ? yDiff * -1 : yDiff;
-            let action = '';
-            
-            if (xDiffPositive <= 20 && yDiffPositive <= 20) {
-                action = 'space';
-            } else if (xDiffPositive > yDiffPositive) {
-                action = xDiff > 0 ? 'arrowRight' : 'arrowLeft';
-            } else if (yDiffPositive >= xDiffPositive) {
-                action = yDiff > 0 ? 'arrowDown' : 'arrowUp';
-            }
-            
-            for (const callBackMethod of this.callbacks) {
-                callBackMethod(this.type, { key: action });
+        if (isPresentationIframe(event.view.location.href)) {
+            this.touchPosition[event.type] = {
+                clientX: event.changedTouches[0].clientX,
+                clientY: event.changedTouches[0].clientY
+            };
+    
+            if (forward) {
+                const xDiff = this.touchPosition.touchstart.clientX - this.touchPosition.touchend.clientX;
+                const yDiff = this.touchPosition.touchstart.clientY - this.touchPosition.touchend.clientY;
+                const xDiffPositive = xDiff < 0 ? xDiff * -1 : xDiff;
+                const yDiffPositive = yDiff < 0 ? yDiff * -1 : yDiff;
+                let action = '';
+                
+                if (xDiffPositive <= 20 && yDiffPositive <= 20) {
+                    action = 'space';
+                } else if (xDiffPositive > yDiffPositive) {
+                    action = xDiff > 0 ? 'arrowRight' : 'arrowLeft';
+                } else if (yDiffPositive >= xDiffPositive) {
+                    action = yDiff > 0 ? 'arrowDown' : 'arrowUp';
+                }
+                
+                for (const callBackMethod of this.callbacks) {
+                    callBackMethod(this.type, { key: action });
+                }
             }
         }
     }
