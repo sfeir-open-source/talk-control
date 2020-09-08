5eda79415a59a3185716c2ae1efff9e6e140585b
Gildas Morel des Vallons <gildas2k@gmail.com>
2020-09-08 16:10:02 +0200

    refactor: rewrite context service

--- src/common/services/context.js
+++ src/common/services/context.js
@@ -1,29 +1,33 @@
+'use strict';
+
 import config from '@config/config.json';
 
-/**
- * Return true if windowLocationHref is presentation's iframe
- *
- * @param {string} windowLocationHref - href string to test
- * @returns {boolean} true if windowLocationHref is presentation's iframe
- */
-export const isPresentationIframe = windowLocationHref => {
-    if (!windowLocationHref) {
-        return false;
+module.exports = {
+    /**
+     * Return true if windowLocationHref is presentation's iframe
+     *
+     * @param {string} windowLocationHref - href string to test
+     * @returns {boolean} true if windowLocationHref is presentation's iframe
+     */
+    isPresentationIframe(windowLocationHref) {
+        if (!windowLocationHref) {
+            return false;
+        }
+    
+        return windowLocationHref.indexOf(`:${config.tcController.port}`) === -1;
+    },
+
+    /**
+     * Return true if windowLocationHref is a remote url
+     *
+     * @param {string} windowLocationHref - href string to test
+     * @returns {boolean} true if windowLocationHref is a remote url
+     */
+    isUsingRemoteUrl(windowLocationHref) {
+        if (!windowLocationHref) {
+            return false;
+        }
+    
+        return windowLocationHref.indexOf('://localhost:') === -1
     }
-
-    return windowLocationHref.indexOf(`:${config.tcController.port}`) === -1;
-};
-
-/**
- * Return true if windowLocationHref is a remote url
- *
- * @param {string} windowLocationHref - href string to test
- * @returns {boolean} true if windowLocationHref is a remote url
- */
-export const isUsingRemoteUrl = windowLocationHref => {
-    if (!windowLocationHref) {
-        return false;
-    }
-
-    return windowLocationHref.indexOf('://localhost:') === -1
 };
