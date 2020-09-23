ce588d610db210c16bc2c319abd8315fa0393775
jefbinomed <jean.francois.garreau@gmail.com>
2020-09-23 16:41:18 +0200

    feat: add fontawesome to add it to dropdown menu

--- src/client/web-components/menu-navigation/menu-navigation.js
+++ src/client/web-components/menu-navigation/menu-navigation.js
@@ -2,7 +2,8 @@
 import '@webcomponents/webcomponentsjs/webcomponents-loader';
 import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter';
 import bulmaStyle from '@granite-elements/granite-lit-bulma/granite-lit-bulma';
-import { LitElement, html } from 'lit-element';
+import { LitElement, html, css } from 'lit-element';
+import Fontawesome from 'lit-fontawesome';
 
 // Extend the LitElement base class
 class MenuNavigation extends LitElement {
@@ -12,7 +13,14 @@ class MenuNavigation extends LitElement {
 
     static get styles() {
         return [
-            bulmaStyle
+            Fontawesome,
+            bulmaStyle,
+            css`
+                #menuDropdown {
+                    position: absolute;
+                    right: 0;
+                }
+            `
         ];
     }
 
@@ -32,23 +40,19 @@ class MenuNavigation extends LitElement {
             <div id="menuDropdown" class="dropdown">
                 <div class="dropdown-trigger">
                     <button id="menuButton" class="button" aria-haspopup="true" aria-controls="dropdown-menu">
-                        <span class="is-hidden-tablet">M</span>
+                        <span class="is-hidden-tablet"><i class="fas fa-caret-down"></i></span>
                         <span class="is-hidden-mobile">Navigation</span>
                     </button>
                 </div>
                 <div class="dropdown-menu" id="dropdown-menu" role="menu">
                     <div class="dropdown-content">
-                        <a href="../../tc-controller/index.html" class="dropdown-item">
-                            Accueil
-                        </a>
-                        <a href="../on-stage/on-stage.html" class="dropdown-item">
-                            On stage view
-                        </a>
+                        <a href="../../tc-controller/index.html" class="dropdown-item"> <i class="fas fa-home"></i> Home </a>
+                        <a href="../on-stage/on-stage.html" class="dropdown-item"> <i class="fas fa-desktop"></i> On stage view </a>
                         <a href="../presenter/presenter.html" class="dropdown-item is-hidden-mobile">
-                            Presenter view
+                            <i class="fas fa-chalkboard-teacher"></i> Presenter view
                         </a>
                         <a href="../presenter/presenter-mobile.html" class="dropdown-item is-hidden-tablet">
-                            Presenter view
+                            <i class="fas fa-mobile-alt"></i> Presenter view
                         </a>
                     </div>
                 </div>
