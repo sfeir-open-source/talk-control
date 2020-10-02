34df3fc41c5e0d37be431a8ed80c8ed191540b52
Gildas Morel des Vallons <gildas2k@gmail.com>
2020-10-02 11:28:57 +0200

    feat: add view-selector component

---
+++ src/client/web-components/view-selector/view-selector.js
@@ -0,0 +1,79 @@
+import '@webcomponents/webcomponentsjs/webcomponents-loader';
+import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter';
+import { bulmaStyles } from '@granite-elements/granite-lit-bulma/granite-lit-bulma';
+import { LitElement, html } from 'lit-element';
+import Fontawesome from 'lit-fontawesome';
+
+class ViewSelector extends LitElement {
+    static get properties() {
+        return {};
+    }
+
+    static get styles() {
+        return [
+            bulmaStyles,
+            Fontawesome
+        ];
+    }
+
+    constructor() {
+        super();
+    }
+
+    firstUpdated() {
+        super.firstUpdated();
+    }
+
+    render() {
+        return html`
+            <div class="columns">
+                <div class="column is-one-fifth">
+                    <a href="/layouts/on-stage/on-stage.html">
+                        <div id="onStageButton" class="card">
+                            <div class="card-content has-text-centered">
+                                <p class="title">
+                                    <i class="fas fa-chalkboard"></i>
+                                </p>
+                                <p class="subtitle">
+                                    Stage view
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="column is-one-fifth is-hidden-tablet">
+                    <!-- Mobile version -->
+                    <a href="/layouts/presenter/presenter-mobile.html">
+                        <div class="card">
+                            <div class="card-content has-text-centered" onclick="window.location.href = '/layouts/presenter/presenter-mobile.html'">
+                                <p class="title">
+                                    <i class="fas fa-chalkboard-teacher"></i>
+                                </p>
+                                <p class="subtitle">
+                                    Presenter view
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="column is-one-fifth is-hidden-mobile">
+                    <!-- Desktop version -->
+                    <a href="/layouts/presenter/presenter.html">
+                        <div class="card">
+                            <div class="card-content has-text-centered" onclick="window.location.href = '/layouts/presenter/presenter.html'">
+                                <p class="title">
+                                    <i class="fas fa-chalkboard-teacher"></i>
+                                </p>
+                                <p class="subtitle">
+                                    Presenter view
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        `;
+    }
+}
+
+customElements.define('tc-view-selector', ViewSelector);
