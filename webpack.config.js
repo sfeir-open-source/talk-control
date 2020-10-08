6e6998f1293e65d3e2432064e2414049dad99d44
Gildas Morel des Vallons <gildas2k@gmail.com>
2020-10-08 15:01:11 +0200

    fix: replace dist-webpack with dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,7 @@ module.exports = {
     },
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist-webpack')
+        path: path.resolve(__dirname, 'dist')
     },
     resolve: {
         alias: {
