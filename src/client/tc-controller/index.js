a784aab848812e603d781ae279d51562592d2aed
Gildas Morel des Vallons <gildas2k@gmail.com>
2020-10-08 11:40:56 +0200

    fix: tc-controller index requires

--- src/client/tc-controller/index.js
+++ src/client/tc-controller/index.js
@@ -2,11 +2,10 @@
 
 require('./index.css');
 require('bulma/css/bulma.min.css');
-require('@fortawesome/fontawesome-free/js/all.min.js');
+require('lit-fontawesome/css/font.css');
 require('../web-components/url-form/url-form.js');
-
-import config from '@config/config.json';
-const QRCode = require('qrcode');
+require('../web-components/view-selector/view-selector');
+require('../web-components/remote-control/remote-control');
 
 window.addEventListener('DOMContentLoaded', function() {
     document.querySelector('tc-url-form').classList.remove('is-hidden');
