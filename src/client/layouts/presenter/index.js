21b5fc375f99bcd66530a36d6839b6b988248d83
Gildas Morel des Vallons <gildas2k@gmail.com>
2020-10-08 15:22:04 +0200

    fix: requires path

--- src/client/layouts/presenter/index.js
+++ src/client/layouts/presenter/index.js
@@ -3,15 +3,15 @@
 require('./index.css');
 require('bulma/css/bulma.min.css');
 require('lit-fontawesome/css/font.css');
-require('../../web-components/slide-view/slide-view.js');
-require('../../web-components/url-form/url-form.js');
-require('../../web-components/clock/clock.js');
-require('../../web-components/timer/timer.js');
-require('../../web-components/notes/notes.js');
-require('../../web-components/menu-navigation/menu-navigation.js');
-require('../../web-components/menu-plugins/menu-plugins.js');
-require('../../../plugins/input/touch-pointer/components/touch-pointer-settings.js');
-require('../../../plugins/input/touch-pointer/components/touch-pointer-mask.js');
+require('@client/web-components/slide-view/slide-view.js');
+require('@client/web-components/url-form/url-form.js');
+require('@client/web-components/clock/clock.js');
+require('@client/web-components/timer/timer.js');
+require('@client/web-components/notes/notes.js');
+require('@client/web-components/menu-navigation/menu-navigation.js');
+require('@client/web-components/menu-plugins/menu-plugins.js');
+require('@plugins/input/touch-pointer/components/touch-pointer-settings.js');
+require('@plugins/input/touch-pointer/components/touch-pointer-mask.js');
 
 import { TCController } from '@client/tc-controller/tc-controller.js';
 import config from '@config/config.json';
--- src/client/tc-controller/index.js
+++ src/client/tc-controller/index.js
@@ -3,9 +3,9 @@
 require('./index.css');
 require('bulma/css/bulma.min.css');
 require('lit-fontawesome/css/font.css');
-require('../web-components/url-form/url-form.js');
-require('../web-components/view-selector/view-selector');
-require('../web-components/remote-control/remote-control');
+require('@client/web-components/url-form/url-form.js');
+require('@client/web-components/view-selector/view-selector');
+require('@client/web-components/remote-control/remote-control');
 
 window.addEventListener('DOMContentLoaded', function() {
     document.querySelector('tc-url-form').classList.remove('is-hidden');
