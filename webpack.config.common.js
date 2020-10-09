86f6c4fcb3de59a5165bebd2d20502dc663dd003
Julien Tanguy <tanguy.j@sfeir.com>
2020-10-09 10:39:54 +0200

    chore: Upgrade to webpack 5

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -46,8 +46,8 @@ module.exports = {
         ]
     },
     output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        publicPath: '', // Required by MiniCssExtractPlugin, but can be overridden dynamically (see tc-component/index.js)
+        filename: '[name].bundle.js'
     },
     resolve: {
         alias: {
