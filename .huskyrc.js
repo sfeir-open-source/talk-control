619c9733ba337642a476800c5dd22072c4836198
Gildas Morel des Vallons <gildas2k@gmail.com>
2020-10-13 17:07:09 +0200

    fix: reactivate husky config

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,8 +2,8 @@ const task = arr => arr.join(' && ');
 
 module.exports = {
     hooks: {
-        //'pre-commit': task(['eslint .', 'pretty-quick --staged']),
-        //'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS'
-        //'pre-push': 'npm test'
+        "pre-commit": "npm run lint",
+        "commit-msg": "commitlint -E HUSKY_GIT_PARAMS",
+        "pre-push": "npm test"
     }
 };
