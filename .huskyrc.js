0a1371ccb2a171242c26ddb48a11de246f06b11f
Gildas Morel des Vallons <gildas2k@gmail.com>
2020-10-13 17:51:01 +0200

    fix: remove pre push rule

--- .huskyrc.js
+++ .huskyrc.js
@@ -3,7 +3,6 @@ const task = arr => arr.join(' && ');
 module.exports = {
     hooks: {
         "pre-commit": "npm run lint",
-        "commit-msg": "commitlint -E HUSKY_GIT_PARAMS",
-        "pre-push": "npm test"
+        "commit-msg": "commitlint -E HUSKY_GIT_PARAMS"
     }
 };
