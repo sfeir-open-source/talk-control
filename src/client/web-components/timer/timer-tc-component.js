51c67f6b50fd8ff3ff05e25a70e8efc8107d3961
Tarik IRHBOULA <tarik.irhboula@gmail.com>
2020-12-10 21:34:08 +0100

    refactor: timerTcComponent reference timer directly (reset)

--- src/client/web-components/timer/timer-tc-component.js
+++ src/client/web-components/timer/timer-tc-component.js
@@ -1,7 +1,12 @@
 import { EventBusComponent } from '@event-bus/event-bus-component';
 
 export class TimerTCComponent extends EventBusComponent {
+    constructor(timer) {
+        super();
+        this.timer = timer;
+    }
+
     init() {
-        postMessage({ type: 'initTimer' });
+        this.timer.reset();
     }
 }
