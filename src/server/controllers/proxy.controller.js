71a1cb809454948a2b541b7542845afa0b1f88ca
Tarik IRHBOULA <tarik.irhboula@gmail.com>
2021-01-01 01:02:28 +0100

    test: proxy controller test suite

--- src/server/controllers/proxy.controller.js
+++ src/server/controllers/proxy.controller.js
@@ -6,13 +6,25 @@ import httpProxy from 'http-proxy';
 const router = express.Router();
 const proxy = httpProxy.createProxyServer();
 
-router.all('*', (req, res) => {
+router.all('*', (req, res) => forwardTraffic(req, res, proxy));
+
+/**
+ * @param req
+ * @param res
+ * @param proxy
+ */
+export function forwardTraffic(req, res, proxy) {
     const presentationUrl = req.cookies && req.cookies['tc-presentation-url'];
-    if (presentationUrl) {
-        req.url = req.originalUrl.replace('/proxy', '');
-        const target = new URL(presentationUrl).origin;
-        proxy.web(req, res, { target, secure: false });
+    let target;
+    try {
+        target = new URL(presentationUrl).origin;
+    } catch (e) {
+        res.send('Invalid presentation URL', 400);
+        return;
     }
-});
+
+    req.url = req.originalUrl.replace('/proxy', '');
+    proxy.web(req, res, { target, secure: false });
+}
 
 export default router;
